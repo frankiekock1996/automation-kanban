ab0daed24ea3cd52549a38363856337dc7056dea
frankiekock1996 <frankiekmklol1996@gmail.com>
2025-04-30 08:22:29 +0800

    Add my new feature

--- src/app/(main)/(pages)/workflows/_components/workflow.tsx
+++ src/app/(main)/(pages)/workflows/_components/workflow.tsx
@@ -29,55 +29,54 @@ const Workflow = ({ description, id, name, publish,boardId }: Props) => {
 //     if (response) toast.message(response)
 //   }
 
-  return (
-    <Card className="flex w-full items-center justify-between">
-      <CardHeader className="flex flex-col gap-4">
-      <Link href={`/workflows/kanban/${boardId}`}>
-        {/* <Link href={`/workflows/editor/${id}`}> */}
-          <div className="flex flex-row gap-2">
-            <Image
-              src="/googleDrive.png"
-              alt="Google Drive"
-              height={30}
-              width={30}
-              className="object-contain"
-            />
-            <Image
-              src="/notion.png"
-              alt="Google Drive"
-              height={30}
-              width={30}
-              className="object-contain"
-            />
-            <Image
-              src="/discord.png"
-              alt="Google Drive"
-              height={30}
-              width={30}
-              className="object-contain"
-            />
-          </div>
-          <div className="">
-            <CardTitle className="text-lg">{name}</CardTitle>
-            <CardDescription>{description}</CardDescription>
-          </div>
-        </Link>
-      </CardHeader>
-      <div className="flex flex-col items-center gap-2 p-4">
-        <Label
-          htmlFor="airplane-mode"
-          className="text-muted-foreground"
-        >
-          {publish! ? 'On' : 'Off'}
-        </Label>
-        <Switch
-          id="airplane-mode"
-          // onClick={onPublishFlow}
-          // defaultChecked={publish!}
-        />
-      </div>
-    </Card>
-  )
+return (
+  <Card className="flex w-full items-center justify-between">
+    <CardHeader className="flex flex-col gap-4">
+      <Link href={`/board/${id}`}>
+        <div className="flex flex-row gap-2">
+          <Image
+            src="/googleDrive.png"
+            alt="Google Drive"
+            height={30}
+            width={30}
+            className="object-contain"
+          />
+          <Image
+            src="/notion.png"
+            alt="Google Drive"
+            height={30}
+            width={30}
+            className="object-contain"
+          />
+          <Image
+            src="/discord.png"
+            alt="Google Drive"
+            height={30}
+            width={30}
+            className="object-contain"
+          />
+        </div>
+        <div className="">
+          <CardTitle className="text-lg">{name}</CardTitle>
+          <CardDescription>{description}</CardDescription>
+        </div>
+      </Link>
+    </CardHeader>
+    <div className="flex flex-col items-center gap-2 p-4">
+      <Label
+        htmlFor="airplane-mode"
+        className="text-muted-foreground"
+      >
+        {publish! ? 'On' : 'Off'}
+      </Label>
+      <Switch
+        id="airplane-mode"
+        // onClick={onPublishFlow}
+        defaultChecked={publish!}
+      />
+    </div>
+  </Card>
+)
 }
 
 export default Workflow
